8a578aedfc30837ff5d63a27a19a0e6b30c501dc
= <wallacemaxters@gmail.com>
2015-12-18 17:51:13 -0200

    :

--- src/SevenFramework/Controller/Controller.php
+++ src/SevenFramework/Controller/Controller.php
@@ -2,18 +2,30 @@
 
 namespace WallaceMaxters\SevenFramework\Controller;
 
+use WallaceMaxters\SevenFramework\Http\Request;
+
 abstract class Controller implements ControllerInterface
 {
+	/**
+	* @var \WallaceMaxters\SevenFramework\Http\Request
+	*/
 	private $request;
 
-	public function getRequest() : Request
-	{
-		return $this->request;
-	}
-
+	/**
+	* @param \WallaceMaxters\SevenFramework\Http\Request $request
+	* @return void
+	*/
 	public function setRequest(Request $request)
 	{
 		$this->request = $request;
+	}
+
+	/**
+	* @return \WallaceMaxters\SevenFramework\Http\Request
+	*/
+	public function getRequest() : Request
+	{
+		return $this->request;
 	}
 
 }
